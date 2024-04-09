5da7d86560ff9b9ac3925470623060c593d0ae68
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-04-09 15:55:32 +0530

    latest changes for live

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "Gym-Website//frontend/",
+  base : "/Gym-Website/",
   plugins: [react()],
 })
